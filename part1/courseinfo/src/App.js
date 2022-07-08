6adc8945ec22204681749910b5f9d582e653b8ec
Josnel Liendo <josnelgabriel@gmail.com>
2022-07-08 11:06:55 -0400

    1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,64 +3,69 @@ const Header = (props) => {
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
-}
+  );
+};
 
 const Part = (props) => {
-  return(
+  return (
     <div>
       <p>
         {props.part}: {props.exercises}
       </p>
     </div>
-    )
-}
+  );
+};
 
 const Content = (props) => {
-  console.log(props.info[0].part);
   return (
-    <div> 
-      <Part part={props.info[0].part} exercises={props.info[0].exercises}/>
-      <Part part={props.info[1].part} exercises={props.info[1].exercises}/>
-      <Part part={props.info[2].part} exercises={props.info[2].exercises}/>
+    <div>
+      <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
     </div>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises: {props.info[0].exercises+props.info[1].exercises+props.info[2].exercises}</p>
+      <p>
+        Number of exercises:{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   // const-definition
-  const course = 'Half Stack application development'
-  // I'm use an array to save the info contents
-  const content = [
-    {
-      part: 'Half Stack application development',
-      exercises: 10, 
-    },
-    {
-      part: 'Using props to pass data',
-      exercises: 7, 
-    },
-    {
-      part: 'State of a component',
-      exercises: 14, 
-    },
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        part: "Half Stack application development",
+        exercises: 10,
+      },
+      {
+        part: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        part: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content info={content} />
-      <Total info={content}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
